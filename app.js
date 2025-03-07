050e8328dbec10e4c98a9b09e113661aed3a45b2
Axamit <axamitta@gmail.com>
2025-03-07 19:21:38 -0300

    Captura el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo");
+    let amigo = nombreAmigo.value;
+
+   
+};
